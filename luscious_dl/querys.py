9c126be1acca7f8e9ad4d8aebc1aa3e70a71b82e
Lucas8x <lucas.poring99@gmail.com>
2021-04-15 10:07:13 -0300

    :art: Prettify Query String

--- luscious_dl/querys.py
+++ luscious_dl/querys.py
@@ -5,11 +5,19 @@ def album_info_query(album_id: str) -> dict:
   :return: Query
   """
   return {
-    "id": 6,
     "operationName": "AlbumGet",
-    "query": "query AlbumGet($id: ID!) {album {get(id: $id) {... on Album {...AlbumStandard} ... on MutationError "
-             "{errors {code message}}}}} fragment AlbumStandard on Album "
-             "{id title created_by number_of_pictures, number_of_animated_pictures}",
+    "query": """query AlbumGet($id: ID!) {
+                  album {
+                    get(id: $id) {
+                      ... on Album {...AlbumStandard}
+                      ... on MutationError {errors {code message}}
+                    }
+                  }
+                }
+                fragment AlbumStandard on Album {
+                  id title created_by number_of_pictures, number_of_animated_pictures
+                }
+    """,
     "variables": {
       "id": album_id
     }
@@ -24,18 +32,30 @@ def album_list_pictures_query(album_id: str, page_number: int) -> dict:
   :return: Query
   """
   return {
-    "id": 7,
     "operationName": "AlbumListOwnPictures",
-    "query": "query AlbumListOwnPictures($input: PictureListInput!) {picture {list(input: $input) {info "
-             "{...FacetCollectionInfo} items {...PictureStandardWithoutAlbum}}}} fragment FacetCollectionInfo on "
-             "FacetCollectionInfo {page has_next_page has_previous_page total_items total_pages items_per_page } "
-             "fragment PictureStandardWithoutAlbum on Picture {url_to_original url_to_video url}",
+    "query": """query AlbumListOwnPictures($input: PictureListInput!) {
+                  picture {
+                    list(input: $input) {
+                      info {...FacetCollectionInfo}
+                      items {...PictureStandardWithoutAlbum}
+                    }
+                  }
+                }
+                fragment FacetCollectionInfo on FacetCollectionInfo {
+                  page has_next_page has_previous_page total_items total_pages items_per_page
+                }
+                fragment PictureStandardWithoutAlbum on Picture {
+                  url_to_original url_to_video url
+                }
+    """,
     "variables": {
       "input": {
-        "filters": [{
-          "name": "album_id",
-          "value": album_id
-        }],
+        "filters": [
+          {
+            "name": "album_id",
+            "value": album_id
+          }
+        ],
         "display": "rating_all_time",
         "page": page_number
       }
@@ -52,13 +72,24 @@ def album_search_query(search_query: str, sorting: str = 'date_trending', page: 
   :return: Query
   """
   return {
-    "id": 12,
     "operationName": "AlbumList",
-    "query": "query AlbumList($input: AlbumListInput!) {album {list(input: $input) {info {...FacetCollectionInfo}"
-             "items {...AlbumMinimal}}}}fragment FacetCollectionInfo on FacetCollectionInfo "
-             "{page has_next_page has_previous_page total_items total_pages items_per_page}"
-             "fragment AlbumMinimal on Album {__typename id title number_of_pictures number_of_animated_pictures "
-             "created_by {id url name display_name user_title} }",
+    "query": """query AlbumList($input: AlbumListInput!) {
+                  album {
+                    list(input: $input) {
+                      info {...FacetCollectionInfo}
+                      items {...AlbumMinimal}
+                    }
+                  }
+                }
+                fragment FacetCollectionInfo on FacetCollectionInfo {
+                  page has_next_page has_previous_page total_items total_pages items_per_page
+                }
+                fragment AlbumMinimal on Album {
+                  __typename id title number_of_pictures number_of_animated_pictures created_by {
+                    id url name display_name user_title
+                  }
+                }
+    """,
     "variables": {
       "input": {
         "display": sorting,
@@ -86,20 +117,31 @@ def user_albums_query(user_id: str, page_number: int) -> dict:
   :return: Query
   """
   return {
-    "id": 8,
     "operationName": "AlbumList",
-    "query": "query AlbumList($input: AlbumListInput!) {album {list(input: $input) {"
-             "info {...FacetCollectionInfo} items {...AlbumMinimal}}}}"
-             "fragment FacetCollectionInfo on FacetCollectionInfo {"
-             "page has_next_page has_previous_page total_items total_pages url_complete}"
-             "fragment AlbumMinimal on Album {id}",
+    "query": """query AlbumList($input: AlbumListInput!) {
+                  album {
+                    list(input: $input) {
+                      info {...FacetCollectionInfo}
+                      items {...AlbumMinimal}
+                    }
+                  }
+                }
+                fragment FacetCollectionInfo on FacetCollectionInfo {
+                  page has_next_page has_previous_page total_items total_pages url_complete
+                }
+                fragment AlbumMinimal on Album {
+                  id
+                }
+    """,
     "variables": {
       "input": {
         "display": "date_newest",
-        "filters": [{
-          "name": "created_by_id",
-          "value": user_id
-        }],
+        "filters": [
+          {
+            "name": "created_by_id",
+            "value": user_id
+          }
+        ],
         "page": page_number
       }
     }
@@ -113,13 +155,21 @@ def user_info_query(user_id: str) -> dict:
   :return: Query
   """
   return {
-    "id": 3,
     "operationName": "ProfileGet",
-    "query": "query ProfileGet($user_id: ID!) {userprofile {get(user_id: $user_id) {... on UserProfile {id user {"
-             "id name display_name user_title } "
-             "number_of_posts number_of_albums number_of_videos is_banned"
-             "filter_settings {uses_default_warnings audience_ids genres_blocked_ids genres_subscribed_ids} }"
-             "... on MutationError {errors {code message}}}}}",
+    "query": """query ProfileGet($user_id: ID!) {
+                  userprofile {
+                    get(user_id: $user_id) {
+                      ... on UserProfile {
+                        id user {id name display_name user_title}
+                        number_of_posts number_of_albums number_of_videos is_banned filter_settings {
+                          uses_default_warnings audience_ids genres_blocked_ids genres_subscribed_ids
+                        }
+                      }
+                      ... on MutationError {errors {code message}}
+                    }
+                  }
+                }
+    """,
     "variables": {
       "user_id": user_id
     }
