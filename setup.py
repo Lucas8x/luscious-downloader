918ab95694bcf3add300e72b43fa5afd51473850
Lucas8x <lucasemanuel8x@gmail.com>
2022-01-06 21:10:05 -0300

    :memo: Add Python 3.10 classifier

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
     'Environment :: Console',
     'Intended Audience :: End Users/Desktop',
     'License :: OSI Approved :: MIT License',
